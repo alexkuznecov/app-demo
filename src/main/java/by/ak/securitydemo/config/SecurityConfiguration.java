f8b07709adcd2a84fe9fa749c7df2fa94fccf402
Alexander Kuznetsov <alexander_kuznetsov@senla.eu>
2020-05-08 01:14:12 +0300

    basic security with role authorization

---
+++ src/main/java/by/ak/securitydemo/config/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package by.ak.securitydemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("admin1")).roles("ADMIN")
+                .and()
+                .withUser("alex").password(passwordEncoder().encode("alex1")).roles("USER")
+                .and()
+                .withUser("ccreat").password(passwordEncoder().encode("ccreat1")).roles("CONTENT");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/index.html").permitAll()
+                .antMatchers("/profile/**").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/movie/**", "/anime/**").hasAnyRole("ADMIN", "CONTENT")
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
